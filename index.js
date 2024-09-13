e2345b86566f7d7059f72cc4c85b58f06b2c8c0c
Pravin Ale <pravinale80@gmail.com>
2024-09-13 09:26:05 +0545

    changes

--- index.js
+++ index.js
@@ -22,12 +22,8 @@ mongoose.connect(MONGO_DB)
 
 
 // Start Server
-// app.listen(5000, () => {
-//     console.log(`Server is running on port ${BASE_URL}`);
-// });
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${BASE_URL}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
